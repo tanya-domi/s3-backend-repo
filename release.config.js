07afd66b9581a0b8ee2618c7a9678e8d0145a877
tanya-domi <sheytanyadominic@gmail.com>
2025-05-17 00:41:02 +0200

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: ["main"], // better as an array
-    repositoryUrl: "https://github.com/your-username/your-repo",
+    repositoryUrl: "https://github.com/tanya-domi/s3-backend-repo.git",
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
